f4e7340e71591c4c5fd7eb16eb2f085db6132b84
Benjaa153 <95550635+Benjaa153@users.noreply.github.com>
2024-03-21 10:39:09 -0300

    Cambios agregados por Benjamin Orellana ------ Nueva carpeta para almacenar las imagenes de (Servicios) de manera local.

--- src/images/index.js
+++ src/images/index.js
@@ -1,21 +1,24 @@
-import menu from "./menu.png"
-import theme from "./theme.png"
-import señoras from "./señoras.png"
-import close from "./delete.png"
-import musc1 from './salamusculacion1.jpg'
-import musc2 from './salamusculacion2.png'
-import musc3 from './salamusculacion3.jpg'
-import cardio1 from './salacardio1.jpg'
-import cardio2 from './salacardio2.jpg'
-import cardio3 from './salacardio3.jpg'
-import yoga1 from './salayoga1.webp'
-import yoga2 from './salayoga2.jpeg'
-import yoga3 from './salayoga3.jpg'
-import cross1 from './salacrossfit1.jpg'
-import cross2 from './salacrossfit2.jpg'
-import cross3 from './salacrossfit3.jpeg'
-import guionesnar from './guionesnar.png'
-import mobileapp from './app.png'
+import menu from './menu.png';
+import theme from './theme.png';
+import señoras from './señoras.png';
+import close from './delete.png';
+import musc1 from './Actividades/act-Musculacion.jpg';
+import musc2 from './Actividades/act-Musculacion2.jpg';
+import musc3 from './Actividades/act-Musculacion3.jpg';
+import cardio1 from './Actividades/act-Cardio.jpg';
+import cardio2 from './Actividades/act-Cardio2.jpg';
+import cardio3 from './Actividades/act-Cardio3.jpg';
+import grupales1 from './Actividades/act-Grupales.jpg';
+import grupales2 from './Actividades/act-Grupales2.jpg';
+import grupales3 from './Actividades/act-Grupales3.jpg';
+import bajoImpacto1 from './Actividades/act-BajoImpacto.jpg';
+import bajoImpacto2 from './Actividades/act-BajoImpacto2.jpg';
+import bajoImpacto3 from './Actividades/act-BajoImpacto3.jpg';
+import kids1 from './Actividades/act-Kids.jpg';
+import kids2 from './Actividades/act-Kids2.jpg';
+import kids3 from './Actividades/act-Kids3.jpg';
+import guionesnar from './guionesnar.png';
+import mobileapp from './app.png';
 
 export {
   menu,
@@ -28,12 +31,15 @@ export {
   cardio1,
   cardio2,
   cardio3,
-  yoga1,
-  yoga2,
-  yoga3,
-  cross1,
-  cross2,
-  cross3,
+  grupales1,
+  grupales2,
+  grupales3,
+  bajoImpacto1,
+  bajoImpacto2,
+  bajoImpacto3,
+  kids1,
+  kids2,
+  kids3,
   guionesnar,
   mobileapp
-}
+};
