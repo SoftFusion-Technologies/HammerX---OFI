8ae47f30e0bd121da3ad5950eb4a0ec9e9b368d6
Rafael Peralta <124094001+Rafaelperalta01@users.noreply.github.com>
2024-03-21 17:08:07 -0300

    Imagenes de comercios local

--- src/components/header/Marcas.jsx
+++ src/components/header/Marcas.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import comercio1 from '../../images/marcas/comercio1.png'
+import comercio2 from '../../images/marcas/comercio2.png'
+import comercio3 from '../../images/marcas/comercio3.png'
+import comercio4 from '../../images/marcas/comercio4.png'
+import comercio5 from '../../images/marcas/comercio5.png'
+import comercio6 from '../../images/marcas/comercio6.png'
+import comercio7 from '../../images/marcas/comercio7.png'
+import soft from '../../images/marcas/soft.jpeg'
 
 const Marcas = () => {
   const settings = {
@@ -46,29 +54,32 @@ const Marcas = () => {
       
         <Slider className=' ' {...settings}>
         <div  title="sin página" className='mx-auto'>
-          <img className="h-6" src="https://www.hammer.ar/image/comercio1.png" alt="" />
+          <img className="h-10" src={soft} alt="SoftFusion" />
+        </div>
+        <div  title="sin página" className='mx-auto'>
+          <img className="h-6 mt-2" src={comercio1} alt="Arquitectura" />
         </div>
         <div >
           <a className="cursor-pointer" href="https://gerf.com.ar" target="_blank" rel="noreferrer">
-            <img className="h-8 w-auto" src="https://www.hammer.ar/image/comercio2.png" alt="" />
+            <img className="h-8 mt-1" src={comercio2} alt="Gerf" />
           </a>
         </div>
         <div >
           <a className="cursor-pointer" href="https://confiterialaestrella.com" target="_blank" rel="noreferrer">
-            <img className="h-6 w-auto" src="https://www.hammer.ar/image/comercio3.png" alt="" />
+            <img className="h-6 mt-2" src={comercio3} alt="Laestrella" />
           </a>
         </div>
         <div  title="sin página">
-          <img className="h-10 w-auto" src="https://www.hammer.ar/image/comercio4.png" alt="" />
+          <img className="h-10" src={comercio4} alt="Nutripaz" />
         </div>
         <div  title="sin página">
-          <img className="h-8 w-auto" src="https://www.hammer.ar/image/comercio5.png" alt="" />
+          <img className="h-8" src={comercio5} alt="Zelaya" />
         </div>
         <div  title="sin página">
-          <img className="h-10 w-auto " src="https://www.hammer.ar/image/comercio6.png" alt="" />
+          <img className="h-10" src={comercio6} alt="Rubio" />
         </div>
         <div  title="sin página">
-          <img className="h-10  w-auto" src="https://www.hammer.ar/image/comercio7.png" alt="" />
+          <img className="h-10" src={comercio7} alt="Look fit" />
         </div>
       </Slider>
 
