f43a9142116757d43c9a2fc467c1505f2ccff58a
Benjamín orellana <95550635+Benjaa153@users.noreply.github.com>
2024-04-01 15:15:52 +0000

    Se mejoro el APP, y se agrego un componente de carga - Cambios aplicados por Benjamin Orellana 01/04/24

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import React, { lazy, Suspense } from 'react';
+/*
+ * Programador: Benjamin Orellana
+ * Fecha Cración: 01 / 04 / 2024
+ * Versión: 1.0
+ *
+ * Descripción:
+ *  Este archivo (App.jsx) es el componente principal de la aplicación.
+ *  Contiene la configuración de enrutamiento, carga de componentes asíncrona,
+ *  y la lógica para mostrar un componente de carga durante la carga inicial.
+ *  Además, incluye la estructura principal de la aplicación, como la barra de navegación,
+ *  el pie de página y las diferentes rutas para las páginas de la aplicación.
+ *
+ * Tema: Configuración de la Aplicación Principal
+ * Capa: Frontend
+ */
+
+import React, { lazy, Suspense, useState, useEffect , memo} from 'react';
 import {
   BrowserRouter as Router,
   Routes as Rutas,
   Route as Ruta
 } from 'react-router-dom';
-import Navbar from './components/header/Navbar';
-import Footer from './components/footer/Footer';
+import Navbar from './components/header/Navbar'; // Importa el componente de la barra de navegación
+import Footer from './components/footer/Footer'; // Importa el componente del pie de página
 
+// Importa los diferentes componentes de las páginas usando lazy loading para mejorar el rendimiento
 const HomePage = lazy(() => import('./pages/HomePage'));
 const Clients = lazy(() => import('./pages/soycliente/Clients'));
 const AboutUs = lazy(() => import('./pages/quieroconocerlos/AboutUs'));
@@ -14,26 +31,57 @@ const OurTeam = lazy(() => import('./pages/quieroconocerlos/OurTeam'));
 const OurValues = lazy(() => import('./pages/quieroconocerlos/OurValues'));
 const Sedeconcepcion = lazy(() => import('./pages/sedes/Concepcion'));
 const Sedemonteros = lazy(() => import('./pages/sedes/Monteros'));
-const Loading = lazy(() => import('./components/Loading'));
+const Loading = lazy(() => import('./components/Loading')); // Importa el componente de carga
 
-function App() {
+/**
+ * Componente principal de la aplicación.
+ * 
+ * @returns {JSX.Element} Elemento JSX que representa la aplicación.
+ */
+const App = memo(() => {
+  // Estado para controlar si se debe mostrar el componente de carga
+  const [showLoading, setShowLoading] = useState(true);
+
+  // Efecto para ocultar el componente de carga después de un cierto tiempo
+  useEffect(() => {
+    // Establece un temporizador para ocultar el componente de carga después de 2 segundos
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 1700);
+
+    // Limpia el temporizador al desmontar el componente para evitar fugas de memoria
+    return () => clearTimeout(timer);
+  }, []); // Este efecto se ejecuta solo una vez, al montar el componente
+
+  // Renderizado del componente
   return (
     <Router>
-      <Suspense fallback={<Loading />}>
-        <Navbar />
-        <Rutas>
-          <Ruta path="/" element={<HomePage />} />
-          <Ruta path="/clientes" element={<Clients />} />
-          <Ruta path="/nosotros" element={<AboutUs />} />
-          <Ruta path="/nosotros/quienessomos" element={<OurTeam />} />
-          <Ruta path="/nosotros/nuestrosvalores" element={<OurValues />} />
-          <Ruta path="/Concepcion" element={<Sedeconcepcion />} />
-          <Ruta path="/Monteros" element={<Sedemonteros />} />
-        </Rutas>
-        <Footer />
+      {/* Componente de Suspense para manejar la carga de componentes lazy */}
+      <Suspense>
+        {/* Condición para mostrar el componente de carga o el contenido de la aplicación */}
+        {showLoading ? <Loading /> : (
+          <>
+            {/* Barra de navegación */}
+            <Navbar />
+
+            {/* Enrutamiento de las diferentes páginas */}
+            <Rutas>
+              <Ruta path="/" element={<HomePage />} /> {/* Página principal */}
+              <Ruta path="/clientes" element={<Clients />} /> {/* Página de clientes */}
+              <Ruta path="/nosotros" element={<AboutUs />} /> {/* Página "Nosotros" */}
+              <Ruta path="/nosotros/quienessomos" element={<OurTeam />} /> {/* Página "Quiénes somos" */}
+              <Ruta path="/nosotros/nuestrosvalores" element={<OurValues />} /> {/* Página "Nuestros valores" */}
+              <Ruta path="/Concepcion" element={<Sedeconcepcion />} /> {/* Página de la sede de Concepción */}
+              <Ruta path="/Monteros" element={<Sedemonteros />} /> {/* Página de la sede de Monteros */}
+            </Rutas>
+
+            {/* Pie de página */}
+            <Footer />
+          </>
+        )}
       </Suspense>
     </Router>
   );
-}
+});
 
-export default App;
+export default App; // Exporta el componente de la aplicación
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Loading.css';
+import { logo } from "../images/svg/index";
 
 const Loading = () => {
-  return (
-      <div>
-         
-      </div>
-  )
-}
 
-export default Loading
+    return (
+        <>
+          
+            <div className="sk-chase">
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+                <div className="sk-chase-dot"></div>
+            </div>
+            
+            {/* <h1
+                Acomodar este texto y aplicarle TIPOGRAFIA: BIG NOODLE TITLING
+                className="text-simple"
+            >
+                VIVÍ SENSACIONES POSITIVAS SIEMPRE
+            </h1>
+             */}
+              <img src={logo} alt="logo" className="mt-20" /> 
+          
+        </>
+    );
+};
+
+export default Loading;
