23f341b9c3dce8cd56b99dc05fd6797a21979a95
Lucas <albornozlucasariel13@gmail.com>
2024-05-03 12:40:29 -0300

    Se dejó listo el dashboard para trabajar con los fondos de los contenedores

--- src/pages/staff/AdminPage.jsx
+++ src/pages/staff/AdminPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/staff/dashboard.css";
 
 const AdminPage = () => {
   return (
@@ -7,19 +8,33 @@ const AdminPage = () => {
       <nav className="w-full flex items-center py-5 z-20 bg-gradient-to-b from-[#fc4b08] to-orange-500 xl:px-0 sm:px-16 px-6">
         <div className="w-full max-w-7xl mx-auto">
           <h1 className="text-white font-bignoodle tracking-wide text-lg text-center">
-            Bienvenidido <span className="">User</span> ⚙ !
+            Bienvenidido <span className="">User</span> !
           </h1>
         </div>
       </nav>
 
       {/* Hero section*/}
       <section className="relative w-full h-screen mx-auto bg-white">
-        <div className="xl:px-0 sm:px-16 px-6 max-w-7xl mx-auto flex justify-evenly items-center pt-52">
-          <div className="w-[300px] h-[200px] bg-orange-500 items-center">
-            <h1></h1>
+        <div className="xl:px-0 sm:px-16 px-6 max-w-7xl mx-auto grid grid-cols-2 max-sm:grid-cols-1 max-md:gap-y-10 md:gap-10 pt-16 md:w-5/6">
+          <div className="bg-orange-500 text-white font-bignoodle w-[400px] h-[200px] mx-auto flex justify-center items-center rounded-lg">
+            <button className="">
+              Foro de Novedades
+            </button>
           </div>
-          <div className="w-[300px] h-[200px] bg-orange-500 ">
-            <h1></h1>
+          <div className="bg-orange-500 text-white font-bignoodle w-[400px] h-[200px] mx-auto flex justify-center items-center rounded-lg">
+            <button className="">
+              Leads y Contacto
+            </button>
+          </div>
+          <div className="bg-orange-500 text-white font-bignoodle w-[400px] h-[200px] mx-auto flex justify-center items-center rounded-lg">
+            <button className="">
+              Convenios
+            </button>
+          </div>
+          <div className="bg-orange-500 text-white font-bignoodle w-[400px] h-[200px] mx-auto flex justify-center items-center rounded-lg">
+            <button className="">
+              CV's Recibidos
+            </button>
           </div>
         </div>
       </section>
