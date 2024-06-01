7b38c8dc1041a0bef7f8e79c0f15756b49c63cb8
Emir Segovia <95300946+Apache90@users.noreply.github.com>
2024-06-01 12:11:34 -0300

    se actualizo el archivo AuthContext para almacenar la sesion en local storage

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -1,27 +1,47 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const AuthContext = createContext();
 
+
 export const AuthProvider = ({ children }) => {
+  // Definir estados locales para el token de autenticación y el nombre de usuario
   const [authToken, setAuthToken] = useState(null);
   const [userName, setUserName] = useState("");
 
-  const login = (token, username) => { // Ajusta la función login para aceptar el nombre de usuario
+  useEffect(() => {
+    // Obtener el token y el nombre de usuario desde el localStorage
+    const token = localStorage.getItem('authToken');
+    const username = localStorage.getItem('userName');
+    // Si hay un token en el localStorage, establecerlo en el estado local
+    if (token) {
+      setAuthToken(token);
+    }
+    // Si hay un nombre de usuario en el localStorage, establecerlo en el estado local
+    if (username) {
+      setUserName(username);
+    }
+  }, []); // El array vacío asegura que este efecto se ejecute solo una vez al montar el componente
+
+  const login = (token, username) => {
+    // Establecer el token y el nombre de usuario en el estado local
     setAuthToken(token);
-    setUserName(username); // Establece el nombre de usuario
+    setUserName(username);
+    // Guardar el token y el nombre de usuario en el localStorage
     localStorage.setItem('authToken', token);
-    localStorage.setItem('userName', username); // Guarda el nombre de usuario en el almacenamiento local
+    localStorage.setItem('userName', username);
   };
 
   const logout = () => {
+    // Limpiar el token y el nombre de usuario del estado local
     setAuthToken(null);
-    setUserName(""); // Limpia el nombre de usuario al cerrar sesión
+    setUserName("");
+    // Remover el token y el nombre de usuario del localStorage
     localStorage.removeItem('authToken');
     localStorage.removeItem('userName');
   };
 
   return (
-    <AuthContext.Provider value={{ authToken, userName, login, logout }}> {/* Pasa el nombre de usuario al contexto */}
+    <AuthContext.Provider value={{ authToken, userName, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
